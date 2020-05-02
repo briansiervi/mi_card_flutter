fd0d3796b6b153edb6ac342348de1604231fd791
Brian Mazini Siervi <brianmazini@gmail.com>
2020-05-02 16:55:18 -0300

    Section 6 - Exercise 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,50 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+//            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                color: Colors.red,
+                width: 100.0,
+              ),
+              Container(
+                  color: Colors.teal,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        color: Colors.yellow,
+                        width: 100.0,
+                        height: 100.0,
+                      ),
+                      Container(
+                        color: Colors.green,
+                        width: 100.0,
+                        height: 100.0,
+                      ),
+                    ],
+                  )),
+              Container(
+                color: Colors.blue,
+                width: 100.0,
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
